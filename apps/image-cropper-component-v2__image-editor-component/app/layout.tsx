05a5746d50fe75d9220fcf413da3476e6e8d7ef4
OnliestWizard <wizinjaconstruction@gmail.com>
2026-06-24 08:56:00 -0300

    Scaffold web app: image-cropper-component-v2-image-editor-component

---
+++ apps/image-cropper-component-v2__image-editor-component/app/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = { title: "image-cropper-component-v2-image-editor-component" };
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
