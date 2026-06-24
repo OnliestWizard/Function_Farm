9bc3ca0d94881f992fce935abd28234a8f74bfb2
OnliestWizard <wizinjaconstruction@gmail.com>
2026-06-24 08:47:54 -0300

    Scaffold web app: camera-capture-definitive-image-editor-component

---
+++ apps/camera-capture-definitive__image-editor-component/app/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = { title: "camera-capture-definitive-image-editor-component" };
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
